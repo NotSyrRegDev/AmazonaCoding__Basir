0c8e96dc78e332ab9c790f5c9416e17dbfec7e82
Ammar__Hamdi <me@ammar-hamdi.gmail.com>
2022-01-11 11:41:32 -0800

    The News Are Amazing

---
+++ backend/data.js
@@ -0,0 +1,78 @@
+const data = {
+    product: [
+        {
+            _id: "1",
+            name: "Nike Slim Shirt",
+            category: "Shirts",
+            image: "/images/p1.jpg",
+            price: 120,
+            brand: "Nike",
+            countInStock: 10,
+            rating: 4.5,
+            numReviews: 10,
+            description: "High Quality Product"
+        },
+        {
+            _id: "2",
+            name: "Addidas For Naers",
+            category: "Shirts",
+            image: "/images/p2.jpg",
+            price: 78,
+            countInStock: 20,
+            brand: "Nike",
+            rating: 3.5,
+            numReviews: 10,
+            description: "High Quality Product"
+        },
+        {
+            _id: "3",
+            name: "Locaste Free Shirt",
+            category: "Shirts",
+            image: "/images/p3.jpg",
+            price: 320,
+            countInStock: 0,
+            countInStock: 35,
+            brand: "Nike",
+            rating: 4.8,
+            numReviews: 17,
+            description: "High Quality Product"
+        },
+        {
+            _id: "4",
+            name: "Puma Slim Pant",
+            category: "Pants",
+            image: "/images/p4.jpg",
+            countInStock: 0,
+            price: 135,
+            brand: "Puma",
+            rating: 4.5,
+            numReviews: 65,
+            description: "High Quality Product"
+        },
+        {
+            _id: "5",
+            name: "Londi Slepping Pant",
+            category: "Pants",
+            image: "/images/p5.jpg",
+            price: 120,
+            brand: "Nike",
+            rating: 4.5,
+            numReviews: 10,
+            description: "High Quality Product"
+        },
+        {
+            _id: "6",
+            name: "Adidas Fit Pant",
+            category: "Pants",
+            image: "/images/p6.jpg",
+            price: 139,
+            brand: "Adidas",
+            countInStock: 0,
+            rating: 4.5,
+            numReviews: 16,
+            description: "High Quality Product"
+        },
+    ],
+}
+
+export default data
---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import data from './data.js';
+
+const app = express();
+
+
+app.get("/api/products" , (req , res) => {
+    res.send(data.product)
+})
+app.get("/" , (req , res) => {
+    res.send("Server Ready")
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port , () => {
+    console.log(`Server At http://localhost:${port}`)
+});
